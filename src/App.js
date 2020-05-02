5a9811a0a5f607fdf4a331092e25154b9353ccb4
Urszula Ficek <xficek@gmail.com>
2020-05-02 08:55:50 +0200

    start

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {HashRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './components/Home'
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
